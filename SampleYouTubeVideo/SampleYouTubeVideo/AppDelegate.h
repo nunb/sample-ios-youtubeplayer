e5e7909cd4f32f5dc46918f3dc6b852ac3b348ef
kanekomakoto <makoto.berry.kaneko@gmail.com>
2014-10-24 00:20:52 +0900

    commit project

---
+++ SampleYouTubeVideo/SampleYouTubeVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleYouTubeVideo
+//
+//  Created by Makoto Kaneko on 2014/10/23.
+//  Copyright (c) 2014年 quiet branch studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleYouTubeVideo/SampleYouTubeVideo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SampleYouTubeVideo
+//
+//  Created by Makoto Kaneko on 2014/10/23.
+//  Copyright (c) 2014年 quiet branch studio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet YTPlayerView *playerView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
